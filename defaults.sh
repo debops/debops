fe36bd2e30ba2007848f5efa1304896599ede888
Maciej Delmanowski <drybjed@gmail.com>
2014-02-13 19:59:30 +0100

    Add piping, redirection in 'defaults.sh'
    Now you can pipe output of the script to a command or redirect it to
    a file.

--- defaults.sh
+++ defaults.sh
@@ -3,24 +3,34 @@
 # Simple defaults browser for ginas project
 # https://github.com/drybjed/ginas/
 
-# Run without options to browse all defaults
-# Specify list of role names separated by spaces to see only those role
-# defaults
-
 # Script uses 'view' command from vim package as convinient pager
 
 # Tips:
 #  - you can search for "role:" to jump between roles
+#  - run without options to browse all defaults
+#  - specify list of role names separated by spaces to see only those roles
+#  - pipe to a command or file to manipulate output (for example grep)
 
 
-(
-	if [ $# -gt 0 ]; then
-		for role in ${@}; do
+if [ $# -gt 0 ]; then
+	role_list=${@}
+fi
+
+function aggregate_defaults() {
+	if [ -n "${role_list}" ]; then
+		for role in ${role_list}; do
 			cat playbooks/roles/${role}/defaults/main.yml
 		done
 	else
 		cat playbooks/roles/*/defaults/main.yml
 	fi
-) | view '+set ft=yaml' -
+}
 
+if [ -t 1 ]; then
+	# if script is run as standalone, redirect to view
+	( aggregate_defaults ) | view '+set ft=yaml' -
+else
+	# else, send everything to stdout
+	aggregate_defaults
+fi
 
