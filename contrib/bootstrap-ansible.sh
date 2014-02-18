1cc0d4efcc9cafe1d2477af9a23e44595c36bff8
Maciej Delmanowski <drybjed@gmail.com>
2014-02-18 19:14:18 +0100

    Add 'contrib/bootstrap-ansible.sh' script
    This script helps install Ansible on hosts that don't have it already
    installed. Currently only Debian and derivative Linux distributions are
    supported.

---
+++ contrib/bootstrap-ansible.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# bootstrap-ansible.sh: download and build Ansible on Debian host
+# https://github.com/drybjed/ginas/
+
+set -e
+
+# Create temporary directory for build
+build_dir=$(mktemp -d)
+trap "rm -rf ${build_dir}" EXIT
+
+cd ${build_dir}
+
+# Install required packages
+sudo apt-get --no-install-recommends -q -y install git \
+	python-paramiko python-yaml python-jinja2 python-httplib2 \
+	cdbs debhelper dpkg-dev python-support fakeroot sshpass
+
+# Clone Ansible from main project repository (devel branch, default)
+git clone https://github.com/ansible/ansible.git ansible
+cd ansible
+
+# Build Debian package
+make deb
+
+version=$(cat VERSION)
+
+# Install Debian package
+sudo dpkg -i ../ansible_${version}_all.deb
+
+
