7a169b9a97d723745eba78b361a8cc9ed1a0065d
Nick Janetakis <nick.janetakis@gmail.com>
2014-07-10 12:20:19 -0400

    Only touch the /etc/hosts file when a fqdn is not found

--- contrib/bootstrap-ginas.py
+++ contrib/bootstrap-ginas.py
@@ -24,10 +24,6 @@ parser.add_option("-f", "--force-install-ansible",
                   dest="forceinstallansible",
                   help="Forcefully install ansible even if it exists",
                   metavar="True")
-parser.add_option("-d", "--domain-name",
-                  dest="domainname",
-                  help="The domain name added to /etc/hosts",
-                  metavar="DOMAIN_NAME")
 
 (options, args) = parser.parse_args()
 
@@ -40,10 +36,6 @@ class CLI:
 
     if self.options.installpath is None:
       self.log_error("You must supply an installation path, example: -i /tmp/foo")
-      quit()
-
-    if self.options.domainname is None:
-      self.log_error("You must supply a domain name, example: -d {0}.dev".format(self.hostname))
       quit()
 
     self.ginas_install_path = os.path.join(self.options.installpath, "ginas")
@@ -69,25 +61,62 @@ class CLI:
     proc = subprocess.Popen(["ansible --version"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     (out, err) = proc.communicate()
 
-    print out
-
     if ('ansible: not found' in out or self.options.forceinstallansible):
       self.log_task("Install ansible:")
       self.shell("{0}/contrib/bootstrap-ansible.sh".format(self.ginas_install_path))
 
-  def update_etc_hosts(self):
+  def check_fqdn(self):
     etchosts_path = "/etc/hosts"
+    etchosts_temp_path = "/tmp{0}.tmp".format(etchosts_path)
+
+    proc = subprocess.Popen(["hostname -f"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+
+    if "." in out and out[:-1] in open(etchosts_path).read():
+      return
+
+    fqdn = self.get_fqdn_input()
+
+    if len(fqdn) == 0:
+      return
 
     self.log_task("Update {0}".format(etchosts_path))
 
     self.shell("mkdir -p /tmp{0}".format(etchosts_path))
-    self.shell("cp {0} /tmp{0}.tmp".format(etchosts_path))
+    self.shell("cp {0} {1}".format(etchosts_path, etchosts_temp_path))
 
-    fo = open("/tmp{0}.tmp".format(etchosts_path), "a")
-    fo.write("\n127.0.1.1 {0} {1}\n".format(self.options.domainname, self.hostname))
+    fo = open(etchosts_temp_path)
+    etc_hosts = fo.readlines()
     fo.close()
 
-    self.shell("sudo mv /tmp{0}.tmp {0}".format(etchosts_path))
+    localhost = "127.0.0.1"
+    localhost_alternative = "127.0.1.1"
+    modify_line_index = -1
+    replace_line = False
+    fqdn_line = "{0} {1} {2}".format(localhost_alternative, fqdn, self.hostname)
+    for index, line in enumerate(etc_hosts):
+      if line.startswith(localhost_alternative):
+        modify_line_index = index
+        replace_line = True
+        break
+      if line.startswith(localhost):
+        modify_line_index = index + 1
+        break
+
+    if replace_line:
+      etc_hosts[modify_line_index] = fqdn_line
+    else:
+      if modify_line_index > len(etc_hosts):
+        etc_hosts.append(fqdn_line)
+      else:
+        etc_hosts[modify_line_index] = fqdn_line
+
+    fo = open(etchosts_temp_path, "w+")
+    for item in etc_hosts:
+      fo.write("{0}\n".format(item))
+    fo.close()
+
+    self.shell("sudo mv {1} {0}".format(etchosts_path, etchosts_temp_path))
     self.shell("sudo chown root:root {0}".format(etchosts_path))
 
   def update_inventory_group_vars_all(self):
@@ -125,23 +154,40 @@ class CLI:
 
   def success_message(self):
     print
-    self.log_with_color(36, "Everything has been installed successfully, please check out your inventory:")
-    self.log_with_color(35, "cd {0}\n".format(self.ginas_inventory_path))
+    self.color_up(36, "Everything has been installed successfully, please check out your inventory:")
+    self.color_up(35, "cd {0}\n".format(self.ginas_inventory_path))
 
-    self.log_with_color(36, "Your computer has been added to an ansible_controller group in inventory/hosts:")
-    self.log_with_color(35, "Please make sure to check out this file if you do not want this behavior\n")
+    self.color_up(36, "Your computer has been added to an ansible_controllers group in inventory/hosts:")
+    self.color_up(35, "Please make sure to check out this file if you do not want this behavior\n")
 
-    self.log_with_color(36, "Visit the getting started guide to setup your first container:")
-    self.log_with_color(35, "https://github.com/ginas/ginas/blob/master/docs/getting-started-with-ginas.rst#run-the-common-playbook")
+    self.color_up(36, "Visit the getting started guide to setup your first container:")
+    self.color_up(35, "https://github.com/ginas/ginas/blob/master/docs/getting-started-with-ginas.rst#run-the-common-playbook")
 
   def log_task(self, task_name):
-    self.log_with_color(33, task_name)
+    print self.color_up(33, task_name)
 
   def log_error(self, error_message):
-    self.log_with_color(31, error_message)
+    print self.color_up(31, error_message)
 
-  def log_with_color(self, color, message):
-    print "\033[0;{0}m{1}\033[0m".format(color, message)
+  def get_fqdn_input(self):
+    print
+    print self.color_up(31, "A fully qualified domain name (fqdn) was not found in /etc/hosts")
+    print
+    print "To make dns work properly you must supply a fqdn"
+    print "For example: {0} (it needs at least 1 period)".format(self.color_up(33, self.hostname + '.dev'))
+    print
+
+    fqdn = ""
+    while True:
+      fqdn = raw_input(self.color_up(36, "Enter in a fqdn or leave it blank to skip this step: "))
+
+      if len(fqdn) == 0 or "." in fqdn:
+        break
+
+    return fqdn
+
+  def color_up(self, color, message):
+    return "\033[0;{0}m{1}\033[0m".format(color, message)
 
   def shell(self, command):
     subprocess.call(command.split(), shell=False)
@@ -151,9 +197,9 @@ cli = CLI(options, args)
 cli.install_default_apt_packages()
 cli.install_ginas()
 cli.install_ansible()
-cli.update_etc_hosts()
 cli.update_inventory_group_vars_all()
 cli.update_inventory_host_vars_machine()
 cli.update_inventory_hosts()
+cli.check_fqdn()
 cli.success_message()
 
