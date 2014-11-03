9c4b32b50192a1d580344e1464bd9647f0314dfc
Maciej Delmanowski <drybjed@gmail.com>
2014-11-03 01:15:04 +0100

    Remove most exceptions from ipaddr()
    Without the exceptions and with False values instead, ipaddr() function
    can be used recursively.

--- playbooks/filter_plugins/ipaddr.py
+++ playbooks/filter_plugins/ipaddr.py
@@ -49,8 +49,6 @@ def ipaddr(value, query = '', version = False, alias = 'ipaddr'):
                 v = netaddr.IPAddress('::')
                 v.value = int(value)
             except:
-                if query and query not in [ 'bool', 'ipv4', 'v4', 'ipv6', 'v6' ]:
-                    raise errors.AnsibleFilterError(alias + ': not an IP address or network: %s' % value)
                 return False
 
         value = str(v)
@@ -68,17 +66,14 @@ def ipaddr(value, query = '', version = False, alias = 'ipaddr'):
                 try:
                     address, prefix = value.split('/')
                 except:
-                    if query and query not in [ 'bool', 'ipv4', 'v4', 'ipv6', 'v6' ]:
-                        raise errors.AnsibleFilterError(alias + ': not an IP address or network: %s' % value)
                     return False
+
                 try:
                     address.isdigit()
                     address = int(address)
                     prefix.isdigit()
                     prefix = int(prefix)
                 except:
-                    if query and query not in [ 'bool', 'ipv4', 'v4', 'ipv6', 'v6' ]:
-                        raise errors.AnsibleFilterError(alias + ': not an IP address or network: %s' % value)
                     return False
 
                 if version and version == 4:
@@ -96,8 +91,6 @@ def ipaddr(value, query = '', version = False, alias = 'ipaddr'):
                             v.value = address
                             network_init = '::/0'
                         except:
-                            if query and query not in [ 'bool', 'ipv4', 'v4', 'ipv6', 'v6' ]:
-                                raise errors.AnsibleFilterError(alias + ': not an IP address or network: %s' % value)
                             return False
 
                 try:
@@ -106,8 +99,6 @@ def ipaddr(value, query = '', version = False, alias = 'ipaddr'):
                     v.prefixlen = prefix
 
                 except:
-                    if query and query not in [ 'bool', 'ipv4', 'v4', 'ipv6', 'v6' ]:
-                        raise errors.AnsibleFilterError(alias + ': not an IP address or network: %s' % value)
                     return False
 
                 value = str(v)
@@ -260,8 +251,8 @@ def ipaddr(value, query = '', version = False, alias = 'ipaddr'):
         try:
             if v in iplist:
                 return value
-        except Exception, e:
-            raise errors.AnsibleFilterError(alias + ': error: %s' % e)
+        except:
+            return False
 
     else:
         try:
