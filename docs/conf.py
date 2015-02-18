3bf15c6409ebe4734cff66834c9836839b4da2fd
Maciej Delmanowski <drybjed@gmail.com>
2015-02-18 20:24:08 +0100

    Add DebOps logo to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,8 @@ yaml2rst.convert_file('../defaults/main.yml', 'defaults.rst')
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
+templates_path = []
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -109,12 +110,12 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'debops.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
