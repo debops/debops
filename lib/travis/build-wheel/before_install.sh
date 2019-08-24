610884397fb149b7808109f9c0dd66e8a079d5f7
Maciej Delmanowski <drybjed@drybjed.net>
2019-08-24 22:03:22 +0200

    Make sure that pandoc is present before release
    The 'pandoc' package is required to convert the 'README.md' file to
    reStructuredText for PyPI.

--- lib/travis/build-wheel/before_install.sh
+++ lib/travis/build-wheel/before_install.sh
@@ -5,4 +5,5 @@ set -o nounset -o pipefail -o errexit
 # Prepare environment for building the Python packages
 
 sudo apt-get -qq update
+sudo apt-get -yq install python3-pypandoc
 pip3 install --upgrade pip wheel setuptools
