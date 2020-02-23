401642bdc3e11e9fe21e0e91f660a7ad8f09a559
Maciej Delmanowski <drybjed@drybjed.net>
2020-02-23 11:36:08 +0100

    [ansible_plugins] Add copyright, license details

--- ansible/roles/ansible_plugins/library/ldap_attrs.py
+++ ansible/roles/ansible_plugins/library/ldap_attrs.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2016, Peter Sagerson <psagers@ignorare.net>
-# (c) 2016, Jiri Tyr <jiri.tyr@gmail.com>
-# (c) 2017, Alexander Korinek <noles@a3k.net>
+# Copyright (c) 2016, Peter Sagerson <psagers@ignorare.net>
+# Copyright (c) 2016, Jiri Tyr <jiri.tyr@gmail.com>
+# Copyright (c) 2017, Alexander Korinek <noles@a3k.net>
+# Copyright (c) 2019, Maciej Delmanowski <drybjed@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
