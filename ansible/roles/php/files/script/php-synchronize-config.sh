51a8a118145725085ea0e8e977c1826ce6073108
Maciej Delmanowski <drybjed@drybjed.net>
2020-02-28 10:57:39 +0100

    [php] Add copyright and license information

--- ansible/roles/php/files/script/php-synchronize-config.sh
+++ ansible/roles/php/files/script/php-synchronize-config.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Copyright (C) 2016      Mariano Barcia <mariano.barcia@gmail.com>
+# Copyright (C) 2016-2019 Maciej Delmanowski <drybjed@gmail.com>
+# Copyright (C) 2016-2019 DebOps <https://debops.org/>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 set -e
 
 php_version="${1:-5}"
