ee3abb6a11dfa82fbc05214301a199283b1a4237
Maciej Delmanowski <drybjed@drybjed.net>
2020-02-29 10:56:53 +0100

    [rails_deploy] Add copyright and license details

--- ansible/roles/rails_deploy/docs/examples/rails/config/puma.rb
+++ ansible/roles/rails_deploy/docs/examples/rails/config/puma.rb
@@ -1,3 +1,8 @@
+# Copyright (C) 2014      Nick Janetakis <nickjanetakis@gmail.com>
+# Copyright (C) 2014-2019 Maciej Delmanowski <drybjed@gmail.com>
+# Copyright (C) 2015-2019 DebOps Project <http://debops.org/>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # Feel free to experiment with this, 0/16 is a good starting point.
 threads ENV['THREADS_MIN'].to_i, ENV['THREADS_MAX'].to_i
 
