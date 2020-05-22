f1a307a36f68916393a77ec89ec2c3a06d150677
Gabriel Lewertowski <gabriel.lewertowski@trust-in-soft.com>
2020-05-22 23:03:25 +0200

    Allow default inventory in .debops.cfg

--- debops/__init__.py
+++ debops/__init__.py
@@ -164,10 +164,14 @@ def find_playbookpath(config, project_root):
             return playbook_path
 
 
-def find_inventorypath(project_root):
+def find_inventorypath(config, project_root):
     """
     Search Ansible inventory in local directories.
     """
+    user_defined_inventorypath = config.get('ansible defaults', {}) \
+                                       .get('inventory')
+    if user_defined_inventorypath:
+        return user_defined_inventorypath
     for inventory_path in ANSIBLE_INVENTORY_PATHS:
         ansible_inventory = os.path.join(project_root, inventory_path)
         if os.path.isdir(ansible_inventory):
