183cf399b4030c11288995e1edf92818fd68a355
Maciej Delmanowski <drybjed@drybjed.net>
2020-06-23 13:06:01 +0200

    Fix pycodestyle issues and Python tests
    (cherry picked from commit 808b077fb367c02e62b49c8d4683b3d6b752a834)

--- tests/test_config.py
+++ tests/test_config.py
@@ -293,7 +293,8 @@ class TestReadConfigDefaultsForPlattforms(TestCase):
         cfgparser = configparser.SafeConfigParser()
         cfgparser.readfp(cStringIO(debops.config.DEFAULTS))
         self.assertEqual(cfgparser.get('paths', 'data-home'),
-                         '~\\Application Data/debops')
+                         os.path.expanduser(
+                             '~\\Application Data/debops'))
 
     def test_defaults_windows_with_APPDATA(self):
         sys.platform = 'win32'
@@ -310,4 +311,5 @@ class TestReadConfigDefaultsForPlattforms(TestCase):
         cfgparser = configparser.SafeConfigParser()
         cfgparser.readfp(cStringIO(debops.config.DEFAULTS))
         self.assertEqual(cfgparser.get('paths', 'data-home'),
-                         '~/Library/Application Support/debops')
+                         os.path.expanduser(
+                             '~/Library/Application Support/debops'))
