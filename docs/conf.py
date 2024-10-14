0505adf568dc79dbd0c8d010d2e7c418cd07bc4b
Maciej Delmanowski <drybjed@drybjed.net>
2024-10-14 14:32:26 +0200

    [docs] Remove 'html_theme_path' variable
    This variable causes a warning on newer Sphinx versions, which results
    in an error in the CI environment and fails the checks.
    
    (cherry picked from commit eb27e50aa644c868eab3e1c1f28a15838b78c501)

--- docs/conf.py
+++ docs/conf.py
@@ -206,7 +206,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     try:
         import sphinx_rtd_theme
         html_theme = 'sphinx_rtd_theme'
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
         html_theme_options = {
             'collapse_navigation': True,
             'logo_only': False
